52f04cc918c3e05ab234939d2ff56ea5661abe2f
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-04-02 18:32:14 -0500

    Adding first MainWindow files.

---
+++ src/mainwindow.cpp
@@ -0,0 +1,22 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "mainwindow.h"
+
+//static
+MainWindow* MainWindow::m_pMainWindow = nullptr;
+
+MainWindow::MainWindow() {
+  m_pMainWindow = this;
+
+  set_default_size (800, 600);
+}
+
+//static
+MainWindow* MainWindow::get_main_window() {
+  return m_pMainWindow;
+}
+
+MainWindow::~MainWindow() {
+}
