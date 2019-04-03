5ad727a8f599fefb0627fd8237595931d15a1d37
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-04-03 15:48:23 -0500

    Trying to usg Gtk::Builder to create the widgets from *.ui files.

--- src/application.cpp
+++ src/application.cpp
@@ -2,8 +2,8 @@
 #include <config.h>
 
 #include <glib/gi18n.h>
+#include <gtkmm/builder.h>
 #include <signal.h>
-#include <iostream>
 #include "application.hpp"
 
 void GsmApplication::load_settings() {
@@ -16,14 +16,14 @@ GsmApplication::GsmApplication()
 }
 
 Glib::RefPtr<RootWindow> GsmApplication::create_rootwindow() {
-    Glib::RefPtr<RootWindow> rootwindow = RootWindow::get();
+    //Glib::RefPtr<RootWindow> rootwindow = RootWindow::get();
 
-    add_window(*(rootwindow.get()));
+    //add_window(*(rootwindow.get()));
 
-    rootwindow->signal_hide().connect(sigc::bind<Glib::RefPtr<RootWindow>>( sigc::mem_fun(*this,
-            &GsmApplication::on_hide_window), rootwindow ));
+    //rootwindow->signal_hide().connect(sigc::bind<Glib::RefPtr<RootWindow>>( sigc::mem_fun(*this,
+    //        &GsmApplication::on_hide_window), rootwindow ));
 
-    return rootwindow;
+    //return rootwindow;
 }
 
 void GsmApplication::on_hide_window(Glib::RefPtr<RootWindow> window) {
@@ -39,8 +39,18 @@ Glib::RefPtr<GsmApplication> GsmApplication::get () {
 }
 
 void GsmApplication::on_activate() {
-    Glib::RefPtr<RootWindow> rootwindow = create_rootwindow();
-    rootwindow->present();
+    // Glib::RefPtr<RootWindow> rootwindow = create_rootwindow();
+    // rootwindow->present();
+
+    Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create();
+    builder->add_from_file("data/interface.ui");
+
+    Gtk::Window* pWindow = nullptr;
+    builder->get_widget("window", pWindow);
+
+    add_window(*pWindow);
+
+    pWindow->present();
 }
 
 int GsmApplication::on_command_line(const Glib::RefPtr<Gio::ApplicationCommandLine>& command_line) {
