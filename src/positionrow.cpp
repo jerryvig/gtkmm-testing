4d0f128c0a3474fab657fb383eb1ce8efb4816ee
Jerry Vigil <jerry.vigil@mktneutral.com>
2019-04-18 00:25:19 -0500

    updates

--- src/positionrow.cpp
+++ src/positionrow.cpp
@@ -13,7 +13,7 @@ PositionRow::PositionRow(std::string ticker, int share_count, double share_basis
     rowGrid = Glib::RefPtr<Gtk::Grid>::cast_static(rowBuilder->get_object("position_row"));
 
     Glib::RefPtr<Gtk::Label> tickerLabel = Glib::RefPtr<Gtk::Label>::cast_static(rowBuilder->get_object("ticker_label"));
-    tickerLabel->set_text("Some ticker here");
+    tickerLabel->set_text("AAPL");
 
     Glib::RefPtr<Gtk::Label> shareCountLabel = Glib::RefPtr<Gtk::Label>::cast_static(rowBuilder->get_object("share_count_label"));
     shareCountLabel->set_text("1200");
@@ -23,6 +23,18 @@ PositionRow::PositionRow(std::string ticker, int share_count, double share_basis
 
     Glib::RefPtr<Gtk::Label> lastTradeLabel = Glib::RefPtr<Gtk::Label>::cast_static(rowBuilder->get_object("last_trade_label"));
     lastTradeLabel->set_text("99.99");
+
+    Glib::RefPtr<Gtk::Label> shareChangeLabel = Glib::RefPtr<Gtk::Label>::cast_static(rowBuilder->get_object("share_change_label"));
+    shareChangeLabel->set_text("1.12%");
+
+    Glib::RefPtr<Gtk::Label> positionBasisLabel = Glib::RefPtr<Gtk::Label>::cast_static(rowBuilder->get_object("position_basis_label"));
+    positionBasisLabel->set_text("89.04");
+
+    Glib::RefPtr<Gtk::Label> positionValueLabel = Glib::RefPtr<Gtk::Label>::cast_static(rowBuilder->get_object("position_value_label"));
+    positionValueLabel->set_text("12,680.23");
+
+    Glib::RefPtr<Gtk::Label> positionChangeLabel = Glib::RefPtr<Gtk::Label>::cast_static(rowBuilder->get_object("position_change_label"));
+    positionChangeLabel->set_text("+3,810.45");
 }
 
 PositionRow::~PositionRow() {
